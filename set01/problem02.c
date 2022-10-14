a8e7e4f62b129044d5d86f1d2f8f6a8b8c73889e
Saniya Khanum  Khanum <114483109+saniyakhanum@users.noreply.github.com>
2022-10-14 05:11:29 +0000

    problem02

---
+++ set01/problem02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main ()
+{
+  int a, b, sum;
+  printf("Enter two numbers : ");
+  scanf("%d%d",&a,&b);
+  sum=a+b;
+  printf("The sum of two numbers is %d",sum);
+  return 0;
+  
+}
